edff35681dd87e1f6acb81d50ea6a350d3d9e802
nazmul.hyder <nazmul.hyder@synesisit.info>
2022-09-12 12:10:17 +0600

    Add project files.

---
+++ HackerRankSolve/PlusMinus_Esy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Console = System.Console;
+
+namespace HackerRankSolve
+{
+    // SOLVED
+    public class PlusMinus_Esy
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            Result.plusMinus(arr);
+        }
+    }
+
+    class Result
+    {
+
+        /*
+         * Complete the 'plusMinus' function below.
+         *
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static void plusMinus(List<int> arr)
+        {
+            double zeroCount = 0;
+            double posCount = 0;
+            double negCount = 0;
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (arr[i] == 0)
+                    zeroCount++;
+                if (arr[i] > 0)
+                    posCount++;
+                if (arr[i] < 0)
+                    negCount++;
+
+            }
+
+            Console.WriteLine((posCount / arr.Count).ToString("N6"));
+            Console.WriteLine((negCount / arr.Count).ToString("N6"));
+            Console.WriteLine((zeroCount / arr.Count).ToString("N6"));
+        }
+
+    }
+
+
+}
---
+++ HackerRankSolve/Staircase_Esy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolve
+{
+    public class Staircase_Esy
+    {
+        /* ======print======
+         *    #
+         *   ##
+         *  ###
+         */
+       public static void Main(string[] args)
+       {
+
+           Console.WriteLine("Enter the N");
+           int n = Convert.ToInt16(Console.ReadLine());
+           Result.staircase(n);
+           
+       }
+
+       class Result
+       {
+           public static void staircase(int n)
+           {
+               for (int i = n; i > 0; i--)
+               {
+                   for (int j = 1; j <= n; j++)
+                   {
+                       Console.Write(j >= i ? "#" : " ");
+                   }
+                   Console.WriteLine("");
+               }
+            }
+        }
+    }
+}
